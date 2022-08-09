ff7d3d0e5b322cad82be71abdb7044e96a53a1a5
Documenter.jl <documenter@juliadocs.github.io>
2022-08-09 05:08:55 +0000

    build based on 4b1ee92

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ActiveSpaceSolvers","category":"page"},{"location":"#ActiveSpaceSolvers","page":"Home","title":"ActiveSpaceSolvers","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ActiveSpaceSolvers.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ActiveSpaceSolvers]","category":"page"}]
+}
