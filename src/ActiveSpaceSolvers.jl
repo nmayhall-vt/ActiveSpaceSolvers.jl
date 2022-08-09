61cca5d2b06f2fd1912cf62fce6d36f0b955623e
Nick Mayhall <nmayhall@vt.edu>
2022-08-09 18:12:36 -0400

    renamed folder

--- src/ActiveSpaceSolvers.jl
+++ src/ActiveSpaceSolvers.jl
@@ -6,8 +6,10 @@ abstract type Problem end
 
 abstract type Solver end
 
-include("oldCI/StringCI.jl");
+# includes
+include("StringCI/StringCI.jl");
 
+# import stuff so we can extend and export
 import .StringCI.FCIProblem
 import .StringCI.build_H_matrix
 import LinearMaps: LinearMap
