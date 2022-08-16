365c75c623969582bef332fc12198a80fd4e8343
Nick Mayhall <nmayhall@vt.edu>
2022-08-16 10:24:10 -0400

    added h0 to solver eigenvalues

--- test/test_FCI.jl
+++ test/test_FCI.jl
@@ -6,7 +6,7 @@ using Test
 using Arpack
 using NPZ
 
-@testset "FCI" begin
+#@testset "FCI" begin
    
     h0 = npzread("h6_sto3g/h0.npy")
     h1 = npzread("h6_sto3g/h1.npy")
@@ -25,29 +25,28 @@ using NPZ
     # test build_H_matrix
     Hmat = build_H_matrix(ints, ansatz)
     @time e,v = eigs(Hmat, nev = 10, which=:SR)
-    e .+= ints.h0
     ref_e = -3.155304800477 # from pyscf in generate_integrals.py
     @test isapprox(e[1], ref_e , atol=1e-10)
-        
    
     ref = [-3.1553048004765447, -2.849049024311162, -2.5973991507252805]
     
     Hmap = LinearMap(ints, ansatz)
     e, v = eigs(Hmap, nev=3, which=:SR)
-    @test all(isapprox.(e.+ints.h0, ref, atol=1e-10))
-    println(e.+ints.h0)
+    println(e)
+    println(ref)
+    @test all(isapprox.(e, ref, atol=1e-10))
 
 
     Hmap = LinearMap(ints, ansatz)
     e = v' * Matrix(Hmap * v)
     display(e)
-    @test all(isapprox.(diag(e.+ints.h0), ref, atol=1e-10))
+    @test all(isapprox.(diag(e), ref, atol=1e-10))
 
     solver = SolverSettings(nroots=3, package="arpack")
     println(solver)
     solution = solve(ints, ansatz, solver)
     display(solution)
-    @test all(isapprox.(solution.energies.+ints.h0, ref, atol=1e-10))
+    @test all(isapprox.(solution.energies, ref, atol=1e-10))
 
 
     # test 1RDMs
@@ -93,4 +92,4 @@ using NPZ
     # string stuff
     #display(ActiveSpaceSolvers.StringCI.string_to_index("110010"))
     @test ActiveSpaceSolvers.FCI.string_to_index("110010") == 19
-end
+#end
