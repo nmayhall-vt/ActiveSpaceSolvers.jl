44760646a96ff6b14f1568ea2730602a15c77f45
Nicole Braunscheidel <nbraunsc@vt.edu>
2023-02-16 12:05:51 -0500

    run ras tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,6 +4,7 @@ using Test
 @testset "ActiveSpaceSolvers.jl" begin
     include("test_FCI.jl")
     include("test_h4.jl")
-    include("test_ras_lu.jl")
-    include("test_ras_rdms.jl")
+    include("RASCI/test_ras_lu.jl")
+    include("RASCI/test_ras_rdms.jl")
+    include("RASCI/test_RASCI.jl")
 end
