2b3fc902ddab20b5d77f0202b360f56017ce4e04
Nicole Braunscheidel <nbraunsc@vt.edu>
2023-07-27 13:38:44 -0400

    added and cleaned up rasci unit tests

--- test/RASCI/test_RASCI.jl
+++ test/RASCI/test_RASCI.jl
@@ -8,34 +8,9 @@ using Arpack
 using NPZ
 using JLD2
 
-#@load "/Users/nicole/My Drive/code/ActiveSpaceSolvers.jl/test/RASCI/ras_h6/_integrals.jld2"
-@load "/Users/nicole/My Drive/code/ActiveSpaceSolvers.jl/test/RASCI/ras_h6/_ras_solution.jld2"
+@load "ras_h6/_ras_solution.jld2"
 
 #v = abs.(v)
-
-#@testset "RAS Precompute same spin blocks (H6, 3α, 3β), Davidson=true" begin
-#    evals, evecs = fci.RASCI.solve(ints, p, ci_vec, 100, 1, 1e-8, true, true)
-#    evecs = abs.(evecs[:,1])
-#    eval = evals[1]
-#    #@test isapprox(evecs, v, atol=10e-10)
-#    @test isapprox(eval, e[1], atol=10e-13)
-#end
-#
-#@testset "RAS Precompute same spin blocks (H6, 3α, 3β), Davidson=false" begin
-#    evals, evecs = fci.RASCI.solve(ints, p, ci_vec, 100, 1, 1e-8, true, false)
-#    evecs = abs.(evecs[1])
-#    eval = evals[1]
-#    #@test isapprox(evecs, v, atol=10e-10)
-#    @test isapprox(eval, e[1], atol=10e-13)
-#end
-#
-#@testset "RAS Do not precompute same spin blocks (H6, 3α, 3β), Davidson=true" begin
-#    evals, evecs = fci.RASCI.solve(ints, p, ci_vec, 100, 1, 1e-8, false, true)
-#    evecs = abs.(evecs[:,1])
-#    eval = evals[1]
-#    #@test isapprox(evecs, v, atol=10e-10)
-#    @test isapprox(eval, e[1], atol=10e-13)
-#end
 
 @testset "RASCI (H6, 3α, 3β), Davidson=false" begin
     solution = ActiveSpaceSolvers.solve(ints, ras, solver)
