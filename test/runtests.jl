4eb35b488b4c1dd1d9f5198fe1ecb3e269a0647b
Nicole Braunscheidel <nbraunsc@vt.edu>
2023-08-01 13:50:59 -0400

    trying to fix testing so CI tests will pass

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,5 +6,5 @@ using Test
     include("test_h4.jl")
     include("RASCI/test_RASCI.jl")
     include("RASCI/test_ras_rdms.jl")
-    include("RASCI/test_TDMS.jl")
+    include("RASCI/test_ras_TDMs.jl")
 end
